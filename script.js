3be9f7972acef8f293044c218487cd03a0d716b7
bittu9975 <99003738+bittu9975@users.noreply.github.com>
2026-06-01 21:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
+const list = document.getElementById("list");
+let count = 1;
 
+
+function addItems(num) {
+    for (let i = 0; i < num; i++) {
+        const li = document.createElement("li");
+        li.textContent = `List Item ${count++}`;
+        list.appendChild(li);
+    }
+}
+
+
+addItems(10);
+
+
+window.addEventListener("scroll", () => {
+    if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight
+    ) {
+        addItems(2);
+    }
+});
